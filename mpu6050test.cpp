7a10b77ee07b2557507c1d8774d22d2b238e28e9
Tim Wu <tim.wu.0@gmail.com>
2014-02-05 00:41:36 -0800

    Move printf() macro to a util header

--- mpu6050test.cpp
+++ mpu6050test.cpp
@@ -11,7 +11,7 @@
 #include <i2c.h>
 #include <MPU6050.h>
 
-#define printf(...) chprintf((BaseSequentialStream *) &SD1, __VA_ARGS__)
+#include "util.h";
 
 int main(void) {
 	int16_t ax, ay, az, gx, gy, gz;
@@ -28,8 +28,8 @@ int main(void) {
 
 	while (true) {
 		mpu6050.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-		printf("ax = %d, ay = %d, az = %d\r\n", ax, ay, az);
-		printf("gx = %d, gy = %d, gz = %d\r\n", gx, gy, gz);
+		printf("%d, %d, %d\r\n", ax, ay, az);
+//		printf("gx = %d, gy = %d, gz = %d\r\n", gx, gy, gz);
 		chThdSleepMilliseconds(1000);
 	}
 }
---
+++ util.h
@@ -0,0 +1,21 @@
+/*
+ * util.h
+ *
+ *  Created on: Feb 4, 2014
+ *      Author: tim
+ */
+
+#ifndef UTIL_H_
+#define UTIL_H_
+
+#ifndef PORTA
+#define PORTA
+#endif
+
+#ifndef PORTB
+#define PORTB
+#endif
+
+#define printf(...) chprintf((BaseSequentialStream *) &SD1, __VA_ARGS__)
+
+#endif /* UTIL_H_ */
