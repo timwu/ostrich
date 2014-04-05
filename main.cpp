1444cfb2c258e12c650a2ccbb959e5c5ac0ba730
Tim Wu <tim.wu.0@gmail.com>
2014-04-05 16:16:24 -0700

    Link in motors

--- main.cpp
+++ main.cpp
@@ -8,17 +8,38 @@
 #include <hal.h>
 #include "receiver.h"
 #include "power.h"
+#include "motors.h"
+#include "util.h"
+
+#define PITCH_CH 1
+#define ROLL_CH 0
+#define YAW_CH 3
+#define THROTTLE_CH 2
+
+static SerialConfig serialConfig = {
+    UBRR_F(115200),
+    USART_CHAR_SIZE_8
+};
 
 int main(void) {
   halInit();
   chSysInit();
-  sdStart(&SD1, NULL);
+  sdStart(&SD1, &serialConfig);
 
   receiverSetup();
   powerSetup();
+  motorsSetup();
 
   while (1) {
-    chThdSleepSeconds(1);
+    motorsSetControl(receiverGetFloat(PITCH_CH), receiverGetFloat(ROLL_CH),
+        receiverGetFloat(YAW_CH), receiverGetFloat(THROTTLE_CH));
+//    uint16_t throttle = receiverGetRaw(THROTTLE_CH);
+//    uint16_t pitch = receiverGetRaw(PITCH_CH);
+//    uint16_t roll = receiverGetRaw(ROLL_CH);
+//    uint16_t yaw = receiverGetRaw(YAW_CH);
+//    printf("%u, %u, %u, %u\r\n", throttle, pitch, roll, yaw);
+//    printf("%u\r\n", receiverGetRaw(4));
+    chThdSleepMilliseconds(50);
   }
 }
 
